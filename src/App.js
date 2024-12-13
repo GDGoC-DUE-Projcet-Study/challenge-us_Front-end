3edb92469e74edd3b978f1c98e9f0a43786176bf
leejyohan <chyun1529@gmail.com>
2024-12-13 13:00:41 +0900

    ✨ New :  로그인 이후만 페이지 이동 가능능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import React from 'react';
+import React, { useEffect } from "react";
 
 import User from "./pages/User";
 import Todo from "./pages/Todo";
 import SignUp from './pages/Signup';
 import LogIn from './pages/Login';
 
+var userId = localStorage.getItem('id');
 
 function App() {
+
+  useEffect(()=>{
+    if(!userId){
+      window.location.href='/'
+    }
+  },[])
+
   return (
     <React.Fragment>
       <BrowserRouter>
